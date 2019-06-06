05ab25783594b4d9be8ed83ea260551537365b07
Christiana Vandermale <christianavdm@gmail.com>
2019-06-06 22:10:42 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
  end
 
  post '/login' do
-   @user = User.find_by(username: params[:username], password: params[:password])
+   @user = User.find_by(:username => params[:username])
    binding.pry
    if @user && @user.authenticate(params[:password])
      session[:user_id] = @user.id
